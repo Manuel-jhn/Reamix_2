d5ec8ff88620cbdbff821756617cf3c61dba1218
Manuel Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-05-28 22:41:57 +0200

    Projet_Reamix_2019

---
+++ TestReamix/Assets/Mort.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mort : MonoBehaviour
+{
+    public Transform teleporttarget;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (tag == "deadzone")
+        {
+            Debug.Log("Tombe !");
+            player.transform.position = teleporttarget.transform.position;
+        }
+        
+    }
+}
